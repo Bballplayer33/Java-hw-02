ee8859ea720dd7a43012e65b3d5bd40d40af30e1
Earl Adams <earladams2000@gmail.com>
2022-01-18 19:00:42 -0500

    working code

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ var generateBtn = document.querySelector("#generate");
 //password values
 var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+123456789";
 var password = "";
-
 //  a. password length 8 - 128
 //  b. lowercase, uppercase, numbers, special characters
 //2. validate the input 
 //3.generate password based off criteria
 
 function generatePassword() {
+  var userChoice = ("0123456789")
   console.log(generatePassword)
   var userChoice = window.prompt("Enter desired password length", "8-128 characters");
-  var length = userChoice
+  var length = userChoice;
     charset = (values);
     retVal = "";
     
-
     if (!userChoice) {
       return
     }
@@ -34,11 +33,12 @@ function generatePassword() {
       window.alert("Character value too high, please click Generate Password and try again");
       return;
     }
-    else if (userChoice = userChoice) {
-      !userChoice;
-      window.alert("Password length not selected, please try again");
-      return;
-    }
+    
+    //else if (userChoice) {
+    //  !userChoice;
+     // window.alert("Password length not selected, please try again");
+    //  return;
+    //}
      
     if (!window.confirm("Click 'OK' to confirm using special characters")) {
       window.alert("Invalid response, please try again & confirm");
